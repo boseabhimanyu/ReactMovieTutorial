c542259ab1df8e60444c7e34fb83e099bb04dc32
Abhimanyu Bose <boseabhimanyu@gmail.com>
2026-06-30 22:23:09 +0530

    Initial commit Search movies working

---
+++ frontend/README.md
@@ -0,0 +1,15 @@
+## TMDB
+
+
+Fetching popular movie details from TMDB API. The webpage has search movies and marking movies favourite feature.
+
+
+# `.env` file variables
+
+```
+VITE_API_KEY=<API KEY>
+```
+
+```
+VITE_API_URL=https://api.themoviedb.org/3
+```
---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+
+import './css/App.css'
+// import MovieCard from './components/MovieCard';
+import Home from './pages/Home';
+import Favourites from './pages/Favourites';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+
+function App() {
+  const movieNumber = 1;
+
+  return (
+    <div>
+      <NavBar />
+    <main className='main-content'>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/favourites' element={<Favourites />}/>
+      </Routes>
+    </main>
+      </div>
+  );
+}
+
+
+export default App
